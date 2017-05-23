5e0f1575aa00ed5e2f92122c6a66b838a5920ebd
haroldoolivieri <olivierisoares@gmail.com>
2017-05-23 01:35:03 -0300

    first commit

---
+++ src/app/title-component/title-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-component',
+  templateUrl: './title-component.component.html',
+  styleUrls: ['./title-component.component.scss']
+})
+export class TitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
